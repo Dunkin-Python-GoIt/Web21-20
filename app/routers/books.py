e6af3b8b5298c1f0203d0152086935758e9bc84b
Volodymyr <dunkin.v@ajax.systems>
2024-05-16 22:00:10 +0300

    commit

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,72 +1,66 @@
 from typing import Annotated
 
 from fastapi import APIRouter, HTTPException, Path, Depends, Query
-from pydantic import BaseModel, Field
-from sqlalchemy import text
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 
+from ..schemas import ResponseBook
 from ..dependecies import get_token_header
 from ..database import get_db
-from ..models.book import Book as Book_db
+from ..repository.book_model import Book
+from ..repository import book_repo
 
 
-router = APIRouter()
+router = APIRouter(prefix="/books", tags=["books"])
 
 
-class ResponseBook(BaseModel):
-    id: int
-    author: str = Field(title="Author of the book", min_length=3, max_length=15)
-    title: str
-    
-    class Config:
-        orm_mode = True
-
-
-fake_books = [
-        {
-            "id": 1,
-            "author": "Duma", 
-            "title": "3 Mushkiters"
-            },
-        {
-            "id": 2,
-            "author": "Gerbert",
-            "title": "Duna"
-        },
-        {
-            "id": 3,
-            "author": "Duma",
-            "title": "The Count of Monte Cristo"
-        }
-        ]
-
-list_of_books = [ResponseBook.model_validate(book) for book in fake_books]
-
-
-@router.get("/books/{book_id}", dependencies=[Depends(get_token_header)])
-async def get_book(book_id: Annotated[int, Path(title="The id of book to get")]):
-    for book in fake_books:
-        if book["id"] == book_id:
-            return book
+@router.get("/{book_id}", response_model=ResponseBook)
+async def get_book(
+    book_id: Annotated[int, Path(title="The id of book to get")],
+    db: Session = Depends(get_db)
+    ):
+    book = db.get_one(Book, book_id)
+    if book:
+        print(book)
+        return book
     raise HTTPException(status_code=404, detail="Book not found")
 
 
-@router.get("/books/", response_model=list[ResponseBook])
+@router.get("/", response_model=list[ResponseBook])
 async def all_books(db: Session = Depends(get_db),
     author: Annotated[str | None, Query(alias="author", example="author=Duma")] = None,
     title: Annotated[str | None, Query(alias="title")] = None) -> list[ResponseBook]:
-    if author:
-        return [book for book in list_of_books if book.author == author]
+    # if author:
+    #     return [book for book in list_of_books if book.author == author]
         # return [book for book in fake_books if book["author"] == author]
     # if title:
         # return [book for book in fake_books if title in book["title"]]
-    result = db.query(Book_db).all()
-    print(result)
+    result = await book_repo.get_all_books(db)
     # db_list_of_books = [Book.model_validate(item) for item in result]
     return result
 
 
-@router.post("/books/")
-async def add_new_book(book: ResponseBook):
-    fake_books.append(book)
-    return fake_books
+@router.post("/")
+async def add_new_book(
+    book: ResponseBook,
+    db: Session = Depends(get_db)):
+    book = Book(**book.model_dump())
+    try:
+        db.add(book)
+        db.commit()
+        db.refresh(book)
+    except IntegrityError as e:
+        raise HTTPException(status_code=409, detail=repr(e))
+    return book
+
+
+@router.delete("/{book_id}")
+async def delete_book(
+    book_id: Annotated[int, Path(title="The id of book to get")],
+    db: Session = Depends(get_db)
+    ):
+    book = db.get_one(Book, book_id)
+    db.delete(book)
+    db.commit()
+    return {"status": f"deleted {book_id}"}
+
